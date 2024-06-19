c227883223225c6f7dc70eaf5c69859c5a8fce32
Camilo Barreto <csbarreto11@gmail.com>
2024-06-19 01:52:39 +0100

    Adding files

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/a31008527_springcoursework/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.a31008527_springcoursework.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.a31008527_springcoursework.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String newsApiKey = "54e3cad81c8249c1b1527eeebdfeedaa";
+}
---
+++ app/build/generated/source/buildConfig/debug/com/example/a31008527_springcoursework/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.a31008527_springcoursework;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.a31008527_springcoursework";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String newsApiKey = "54e3cad81c8249c1b1527eeebdfeedaa";
+}
